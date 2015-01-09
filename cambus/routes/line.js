ae1429f4e174a7b89052e0576d77e5646178a080
pinetree-kr <vanadate.kr@gmail.com>
2015-01-09 17:40:21 +0900

    ng changed

--- cambus/routes/line.js
+++ cambus/routes/line.js
@@ -4,27 +4,50 @@ var Line = require('../models/line');
 var City = require('../models/city');
 
 router.get('/', function(req, res){
-	/*/
-	Line.find(
-		null,
-		'dept dest distance updated',
-		function(err, lines){
-		if(err) res.send(err);
-		res.send(lines);
-	}).populate('dept', 'name').populate('dest', 'name');
 	/**/
-	Line.find(function(err, lines){
-		if(err) res.send(err);
+	Line.find({})
+	.populate('dept','name pref index')
+	.populate('dest', 'name pref index')
+	.exec(function(err ,lines){
+		if(err) res.status(500).send(err);
 		res.send(lines);
 	});
+	;
 });
 router.get('/:_id', function(req, res){
 	var _id = req.params._id
-	Line.findOne({_id:_id}, function(err, line){
-		if(!err){
+	Line.findOne(
+		{_id:_id},
+		function(err, line){
+			if(err) res.status(500).send(err);
 			res.send(line);
-		}
-	});
+		}).populate('dept', 'name').populate('dest', 'name');
 });
-
+router.put('/:_id', function(req, res){
+	var _id = req.params._id;
+	var line = req.body;
+	var data = {};
+	data.updated = new Date();
+	if(line.dept !== undefined){
+		data.dept = line.dept._id;
+	}
+	if(line.dest !== undefined){
+		data.dest = line.dest._id;
+	}
+	if(line.distance !== undefined){
+		data.distance = line.distance;
+	}
+	//console.log(data);
+	Line.findOneAndUpdate(
+		{
+			_id : _id
+		},
+		{
+			$set : data
+		},
+		function(err){
+			if(err) res.status(500).send(err);
+			res.send('update line');
+		});
+});
 module.exports = router;
--- cambus/www/js/app.js
+++ cambus/www/js/app.js
@@ -1,4 +1,4 @@
-//var url = "http://130.211.242.214:8080"
+var url = "http://130.211.242.214:8080"
 
 var app = angular.module('app', ['ngRoute'])
 .config(function($routeProvider){
@@ -9,6 +9,14 @@ var app = angular.module('app', ['ngRoute'])
 	$routeProvider.when('/line', {
 		templateUrl : 'templates/line-list.html',
 		controller : 'LineListController'
+	});
+	$routeProvider.when('/line/write', {
+		templateUrl : 'templates/line-write.html',
+		controller : 'LineWriteController'
+	});
+	$routeProvider.when('/line/edit/:_id', {
+		templateUrl : 'templates/line-write.html',
+		controller : 'LineEditController'
 	});
 	$routeProvider.when('/city', {
 		templateUrl : 'templates/city-list.html',
@@ -22,69 +30,182 @@ var app = angular.module('app', ['ngRoute'])
 		templateUrl : 'templates/city-write.html',
 		controller : 'CityEditController'
 	});
-	$routeProvider.when('/write', {
-		templateUrl : 'write.html',
-		controller: 'WriteController'
+	$routeProvider.when('/type', {
+		templateUrl : 'templates/type-list.html',
+		controller : 'TypeListController'
 	});
-	$routeProvider.when('/edit/:_id', {
-		templateUrl : 'write.html',
-		controller: 'EditController'
+	$routeProvider.when('/type/write', {
+		templateUrl : 'templates/type-write.html',
+		controller : 'TypeWriteController'
+	});
+	$routeProvider.when('/type/edit/:_id', {
+		templateUrl : 'templates/type-write.html',
+		controller : 'TypeEditController'
+	});
+	$routeProvider.when('/company', {
+		templateUrl : 'templates/company-list.html',
+		controller : 'CompanyListController'
+	});
+	$routeProvider.when('/company/write', {
+		templateUrl : 'templates/company-write.html',
+		controller : 'CompanyWriteController'
+	});
+	$routeProvider.when('/company/edit/:_id', {
+		templateUrl : 'templates/company-write.html',
+		controller : 'CompanyEditController'
 	});
 })
-.controller('LineListController', function($scope, $http){
+.controller('LineListController', function($scope, $http, $filter, $location){
+	var orderBy = $filter('orderBy');
 	$http.get(url+'/api/line').
 		success(function(data){
-			$scope.lines = data;
+			$scope.items = data;
+			$scope.orderBy("-dept.pref", false);
+			$scope.orderBy("dept.index", false);
 		}).
 		error(function(err){
 			console.log(err);
 		});
+	$scope.orderBy = function(predicate, reverse){
+		$scope.items = orderBy($scope.items, predicate, reverse);
+	}
+	$scope.edit = function(_id){
+		$location.path('/line/edit/'+_id);
+	}
 })
-.controller('CityListController', function($scope, $http){
-	$http.get(url+'/api/city').success(function(status, data){
-		$scope.cities = status;
+.controller('LineEditController', function($scope, $routeParams, $http, $location){
+	$http.get(url+'/api/city').success(function(cities){
+		$scope.cities = cities;
+		$http.get(url+'/api/line/' + $routeParams._id).success(function(line){
+			$scope.item = line;
+			for(var i=0; i<$scope.cities.length; i++){
+				if($scope.cities[i]._id === $scope.item.dept._id){
+					$scope.dept = $scope.cities[i];
+				}
+				if($scope.cities[i]._id === $scope.item.dest._id){
+					$scope.dest = $scope.cities[i];
+				}
+				if($scope.dept !== undefined && $scope.dest !== undefined){
+					break;
+				}
+			}
+		});
 	});
-	$scope.deletecity = function(_id){
+	$scope.submit = function(line){
+		$http.put(url+'/api/line/' + $routeParams._id, line).success(function(data){
+			//console.log(data);
+			$location.path('/line');
+		});
+	};
+	/*/
+	$scope.delete = function(_id){
+		$http.delete(url+'/api/city/'+_id).success(function(data){
+			//angular.element(document.getElementById(_id)).remove();
+			$location.path('/city');
+		});
+	}
+	/**/
+})
+.controller('TypeListController', function($scope, $http, $location){
+	$http.get(url+'/api/type').
+		success(function(data){
+			$scope.items = data;
+		}).
+		error(function(err){
+			console.log(err);
+		});
+	$scope.edit = function(_id){
+		$location.path('/type/edit/'+_id);
+	}
+})
+.controller('TypeEditController', function($scope, $routeParams, $http, $location){
+	$http.get(url+'/api/type/' + $routeParams._id).success(function(data){
+		$scope.item = data;
+	});
+	$scope.submit = function(type){
+		$http.put(url+'/api/type', type).success(function(data){
+			//console.log(data);
+			$location.path('/type');
+		});
+	};
+	/*/
+	$scope.delete = function(_id){
+		$http.delete(url+'/api/city/'+_id).success(function(data){
+			//angular.element(document.getElementById(_id)).remove();
+			$location.path('/city');
+		});
+	}
+	/**/
+})
+.controller('CompanyListController', function($scope, $http, $location){
+	$http.get(url+'/api/company').
+		success(function(data){
+			$scope.items = data;
+		}).
+		error(function(err){
+			console.log(err);
+		});
+	$scope.edit = function(_id){
+		$location.path('/company/edit/'+_id);
+	}	
+})
+.controller('CompanyEditController', function($scope, $routeParams, $http, $location){
+	$http.get(url+'/api/company/' + $routeParams._id).success(function(data){
+		$scope.item = data;
+	});
+	$scope.submit = function(company){
+		$http.put(url+'/api/company', company).success(function(data){
+			//console.log(data);
+			$location.path('/company');
+		});
+	};
+	/*/
+	$scope.delete = function(_id){
+		$http.delete(url+'/api/city/'+_id).success(function(data){
+			//angular.element(document.getElementById(_id)).remove();
+			$location.path('/city');
+		});
+	}
+	/**/
+})
+.controller('CityListController', function($scope, $http, $location){
+	$http.get(url+'/api/city').success(function(data){
+		$scope.items = data;
+	});
+	$scope.edit = function(_id){
+		$location.path('/city/edit/'+_id);
+	}
+	/*/
+	$scope.delete = function(_id){
 		$http.delete(url+'/api/city/'+_id).success(function(data){
 			angular.element(document.getElementById(_id)).remove();
 		});
 	}
-})
-.controller('CityListController', function($scope, $http, $location){
-	$http.get('/api/city').success(function(data){
-		$scope.cities = data;
-	});
-	$scope.editCity = function(_id){
-		$location.path('/city/edit/'+_id);
-	}
+	/**/
 })
 .controller('CityWriteController', function($scope, $http, $location){
-	$scope.submitCity = function(city){
+	$scope.submit = function(city){
 		$http.post('/api/city', city).success(function(data){
 			$location.path('/city');
 		});
 	};
 })
 .controller('CityEditController', function($scope, $routeParams, $http, $location){
-	$http.get('/api/city/' + $routeParams._id).success(function(data){
-		//console.log(data);
-		$scope.city = data;
-		/*/
-		if(data.pref===undefined){
-			$scope.city.pref=0;
-		}
-		/**/
+	$http.get(url+'/api/city/' + $routeParams._id).success(function(data){
+		$scope.item = data;
 	});
-	$scope.submitCity = function(city){
-		$http.put('/api/city', city).success(function(data){
+	$scope.submit = function(city){
+		$http.put(url+'/api/city', city).success(function(data){
 			//console.log(data);
 			$location.path('/city');
 		});
 	};
-	$scope.deleteCity = function(_id){
-		$http.delete('/api/city/'+_id).success(function(data){
+	/*/
+	$scope.delete = function(_id){
+		$http.delete(url+'/api/city/'+_id).success(function(data){
 			//angular.element(document.getElementById(_id)).remove();
 			$location.path('/city');
 		});
 	}
+	/**/
 });
