1b4cec6a256c4adae75ea0a6460c3173997e59a3
pinetree-kr <vanadate.kr@gmail.com>
2015-01-09 10:01:06 +0000

    line-bus

--- cambus/routes/line.js
+++ cambus/routes/line.js
@@ -23,6 +23,46 @@ router.get('/:_id', function(req, res){
 			res.send(line);
 		}).populate('dept', 'name').populate('dest', 'name');
 });
+router.get('/bus/:_id', function(req, res){
+	var line_id = req.params._id;
+	var Bus = require('../models/bus');
+	var Type = require('../models/type');
+	var Company = require('../models/company');
+	var async = require('async');
+	
+	//Line-Bus
+	async.waterfall([
+		function(callback){
+			Line
+				.findOne({_id:line_id})
+				.select('_id dept dest distance')
+				.populate('dept', 'name')
+				.populate('dest', 'name')
+				.exec(function(err, line){
+				if(err) send.status(500).send(err);
+				var json = {
+					line : line
+				}
+				callback(null, json);
+			});
+		},
+		function(json, callback){
+			Bus
+				.find({line_id : line_id})
+				.select('_id company_id type_id mids times price transfer international seat rest_area updated')
+				.populate('company_id', 'name')
+				.populate('type_id', 'name')
+				.exec(function(err, buses){
+					if(err) res.status(500).send(err);
+					json['buses'] = buses;
+					res.send(json);
+				});
+		},
+		function(err){
+			if(err) res.status(500).send(err);
+		}
+		]);
+});
 router.put('/:_id', function(req, res){
 	var _id = req.params._id;
 	var line = req.body;
