78b74d600de53b522ae2a9d33f5ef473f12733a4
pinetree-kr <vanadate.kr@gmail.com>
2015-01-14 05:00:56 +0000

    bus edit

--- cambus/routes/bus.js
+++ cambus/routes/bus.js
@@ -19,5 +19,35 @@ router.get('/:_id', function(req, res){
 		}
 	});
 });
+router.put('/:_id', function(req, res){
+	var id = req.params._id;
+	
+	var bus = req.body;
+	var data = {};
 
+	if(bus.mids === undefined){
+		data.mids = bus.mids;
+	}
+	if(bus.times === undefined){
+		data.times = bus.times;
+	}
+	if(bus.price === undefined){
+		data.price = bus.price;
+	}
+	if(bus.seat === undefined){
+		data.seat = bus.seat;
+	}
+	data.updated = new Date();
+
+	Bus.findOneAndUpdate(
+		{_id:id},
+		{
+			$set : data
+		},
+		function(err, result){
+			if(err) res.status(500).send(err);
+			res.send(result);
+		});
+	
+});
 module.exports = router;
